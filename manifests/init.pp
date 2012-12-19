5307c49529bce2cfdf107980d7db48c2fef80cb3
Steffen Zieger <me@saz.sh>
2012-12-20 00:26:22 +0100

    initial version

---
+++ manifests/init.pp
@@ -0,0 +1,46 @@
+# == Class: bridge_utils
+#
+# Manage bridge-utils with Puppet
+#
+# === Parameters
+#
+# [*ensure*]
+#   Ensure if present or absent.
+#   Default: present
+#
+# [*autoupgrade*]
+#   Upgrade package automatically, if there is a newer version.
+#   Default: false
+#
+# [*package*]
+#   Name of the package.
+#   Only set this, if your platform is not supported or you know,
+#   what you're doing.
+#   Default: auto-set, platform specific
+#
+# === Variables
+#
+#  None
+#
+# === Examples
+#
+#  class { bridge_utils: }
+#
+# === Authors
+#
+# Steffen Zieger <me@saz.sh>
+#
+# === Copyright
+#
+# Copyright 2012 Steffen Zieger
+#
+class bridge_utils(
+  $ensure = 'present',
+  $autoupgrade = false,
+  $package = $bridge_utils::params::package
+) inherits bridge_utils::params {
+
+  package { $package:
+    ensure => $package_ensure,
+  }
+}
---
+++ manifests/params.pp
@@ -0,0 +1,14 @@
+class bridge_utils::params {
+  case $::osfamily {
+    debian: {
+      $package = 'bridge-utils'
+    }
+    default: {
+      case $::operatingsystem {
+      }
+      default: {
+        fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
+      }
+    }
+  }
+}
